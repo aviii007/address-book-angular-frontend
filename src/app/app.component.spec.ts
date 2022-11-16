327cc78bcf2bbb7489c7ea1a845b839f5e722d56
aviii007 <kanojia.avi@gmail.com>
2022-11-16 09:29:35 +0530

    Added the header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Address-Book-Angular'`, () => {
+  it(`should have as title 'AddressBookAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Address-Book-Angular');
+    expect(app.title).toEqual('AddressBookAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Address-Book-Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AddressBookAngular app is running!');
   });
 });
